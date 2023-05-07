c28ff9bbdcc1bee2f14ae711b2af101607762ce3
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-05-07 09:11:32 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let btn  = document.getElementById("toggle")
+let btn1  = document.getElementById("toggle1")
+let navbar  = document.getElementById("navbar-responsive")
+btn.addEventListener("click", function(){
+    navbar.classList.toggle("hide");
+})
+btn1.addEventListener("click", function(){
+    navbar.classList.toggle("hide");
+})
